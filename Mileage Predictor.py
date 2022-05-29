9ce501cfd9f2847fd9653f40f9998be4b64531eb
Sneha Jain <87900165+Perpetual-Incantation@users.noreply.github.com>
2022-05-29 13:40:39 +0530

    Update Mileage Predictor.py

--- Mileage Predictor.py
+++ Mileage Predictor.py
@@ -1,243 +1,182 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy.stats import ttest_ind
-import statsmodels.formula.api as smf
-
-
-# In[2]:
-
 def mil(a):
+    car_df = pd.read_csv('auto-mpg.csv')
 
+    car_df.horsepower.unique()
 
+    # ## Observation: In the horsepower values ? mark is included whichmakes the variable as object
+    # ### need to replace the values and convert horsepower variable to integer
 
-  car_df=pd.read_csv('auto-mpg.csv')
+    # In[7]:
 
+    car_df['horsepower'] = car_df['horsepower'].replace('?', np.nan)
 
-  car_df.horsepower.unique()
+    # In[8]:
 
+    # here the ? mark vales are replaced with the nan values so will check how many records got replaced
+    car_df['horsepower'].isnull().sum()
 
-  # ## Observation: In the horsepower values ? mark is included whichmakes the variable as object
-  # ### need to replace the values and convert horsepower variable to integer
+    # #### 6 values got replaced as nan in horsepower variable
+    #
+    # ### now need to convert the horsepower variable to interger and fill the nan values with median value of horsepower
 
-  # In[7]:
+    # In[9]:
 
+    car_df['horsepower'] = car_df['horsepower'].astype('float64')
 
-  car_df['horsepower']=car_df['horsepower'].replace('?',np.nan)
+    # In[10]:
 
+    car_df['horsepower'].dtype
 
-  # In[8]:
+    # In[11]:
 
+    # fill the nan values with median values
+    car_df['horsepower'] = car_df['horsepower'].fillna(car_df['horsepower'].median())
 
-  # here the ? mark vales are replaced with the nan values so will check how many records got replaced
-  car_df['horsepower'].isnull().sum()
+    # In[12]:
 
+    car_df['horsepower'].isnull().sum()
 
-  # #### 6 values got replaced as nan in horsepower variable
-  # 
-  # ### now need to convert the horsepower variable to interger and fill the nan values with median value of horsepower
+    # ### so now the hp variable got converted to integers and now we can proceed to check the correlation of the variables
 
-  # In[9]:
+    # In[13]:
 
+    corr_table = car_df.corr()
+    corr_table
 
-  car_df['horsepower']=car_df['horsepower'].astype('float64')
+    # ## Lets visualize the above values in the heat map
 
+    # In[14]:
 
-  # In[10]:
+    plt.figure(figsize=(20, 10))
+    g = sns.heatmap(corr_table, annot=True)
+    g.set(title='Correlation matrix of the car-mpg dataset')
+    plt.show()
 
+    # ## Inferences:
+    #
+    #  ### Mpg column is having good corelation on cyl,disp,wt
+    #
+    #  ### cyl ,disp and wt  and hp are having negative corelation
+    #
+    #  ### next will check the data distribution using the pair plot
 
-  car_df['horsepower'].dtype
+    # In[15]:
 
+    sns.pairplot(car_df, diag_kind='kde')
+    plt.show()
 
-  # In[11]:
+    # In[16]:
 
+    car_df['cylinders'].value_counts()
 
-  # fill the nan values with median values
-  car_df['horsepower']=car_df['horsepower'].fillna(car_df['horsepower'].median())
+    # # Next will be performing the statistical test to find the significance of variable so that we can reduce no.of variable
 
+    # In[17]:
 
-  # In[12]:
+    # for this we need to import statsmodels as shown below
+    import statsmodels.formula.api as smf
 
+    # In[18]:
 
-  car_df['horsepower'].isnull().sum()
+    test1 = smf.ols('mpg~cylinders+displacement+horsepower+weight+acceleration+origin', car_df).fit()
 
+    # In[19]:
 
-  # ### so now the hp variable got converted to integers and now we can proceed to check the correlation of the variables
+    test1.summary()
 
-  # In[13]:
+    # ## Inference as in the above summary the p value of the acc is greater than 0.05 so we can remove the acc variable from the dataset
 
+    # In[20]:
 
-  corr_table=car_df.corr()
-  corr_table
+    car_df = car_df.drop('acceleration', axis=1)
+    # car_df.head()
 
+    # In[21]:
 
-  # ## Lets visualize the above values in the heat map
+    # i am removing the car name variable as i has nothing to to here
+    car_df = car_df.drop('car name', axis=1)
+    # car_df.head()
 
-  # In[14]:
+    # ## Now we are good enough to train our regression model
 
+    # In[22]:
 
-  plt.figure(figsize=(20,10))
-  g=sns.heatmap(corr_table,annot=True)
-  g.set(title='Correlation matrix of the car-mpg dataset')
-  plt.show()          
-            
+    # importing the requried libaries for regression
 
+    from sklearn.linear_model import LinearRegression
+    from sklearn.model_selection import KFold
+    from sklearn import metrics
 
-  # ## Inferences:
-  #  
-  #  ### Mpg column is having good corelation on cyl,disp,wt
-  #  
-  #  ### cyl ,disp and wt  and hp are having negative corelation 
-  #  
-  #  ### next will check the data distribution using the pair plot
+    # ## now i am spliting my dataframe into dependent and independent variable i.e x and y
 
-  # In[15]:
+    # In[23]:
 
+    y = car_df['mpg']
+    # y.head()
 
-  sns.pairplot(car_df,diag_kind='kde')
-  plt.show()
+    # In[24]:
 
+    x = car_df.iloc[:, 1:]
 
-  # In[16]:
+    # ## I am using here KFold method to train and test my model to get good metrices and performance
 
+    # In[25]:
 
-  car_df['cylinders'].value_counts()
+    kf = KFold(n_splits=5, shuffle=True, random_state=2)
+    rmse = []
+    for train, test in kf.split(x, y):
+        LR = LinearRegression()
+        # print(train)
+        xtrain = x.iloc[train]
+        xtest = x.iloc[test]
+        ytrain = y.iloc[train]
+        ytest = y.iloc[test]
+        LR.fit(xtrain, ytrain)
+        ypredict = LR.predict(xtest)
 
+        rmse.append(np.sqrt(metrics.mean_squared_error(ytest, ypredict)))
 
-  # # Next will be performing the statistical test to find the significance of variable so that we can reduce no.of variable 
+    print('Rmse error in the first test is :%1.3f' % (rmse[0]))
+    print('Rmse error in the second test is:%1.3f' % (rmse[1]))
+    print('Rmse error in the third test is :%1.3f' % (rmse[2]))
+    print('Rmse error in the fourth test is:%1.3f' % (rmse[3]))
+    print('Rmse error in the fifth test is :%1.3f' % (rmse[4]))
 
-  # In[17]:
+    print('Average mean rmse error i.e Bias error is: %1.3f' % (np.mean(rmse)))
 
+    print('Variance of rmse error i.e Variance error is: %1.3f' % (np.var(rmse, ddof=1)))
 
-  # for this we need to import statsmodels as shown below 
-  import statsmodels.formula.api as smf
+    # In[26]:
 
+    ## predicting the mileage values for new values
+    values = [[8, 206, 200, 1900, 70, 2]]
+    new_mileage = LR.predict(values)
+    print('predicted new mileage for above values is %1.3f' % new_mileage)
+    return LR.predict(values)
 
-  # In[18]:
+    # In[27]:
 
 
-  test1=smf.ols('mpg~cylinders+displacement+horsepower+weight+acceleration+origin',car_df).fit()
+import streamlit as st
 
-
-  # In[19]:
-
-
-  test1.summary()
-
-
-  # ## Inference as in the above summary the p value of the acc is greater than 0.05 so we can remove the acc variable from the dataset
-
-  # In[20]:
-
-
-  car_df=car_df.drop('acceleration',axis=1)
-  # car_df.head()
-
-
-  # In[21]:
-
-
-  # i am removing the car name variable as i has nothing to to here
-  car_df=car_df.drop('car name',axis=1)
-  # car_df.head()
-
-
-  # ## Now we are good enough to train our regression model
-
-  # In[22]:
-
-
-  # importing the requried libaries for regression
-
-  from sklearn.linear_model import LinearRegression
-  from sklearn.model_selection import KFold
-  from sklearn import metrics
-
-
-  # ## now i am spliting my dataframe into dependent and independent variable i.e x and y
-
-  # In[23]:
-
-
-  y=car_df['mpg']
-  # y.head()
-
-
-  # In[24]:
-
-
-  x=car_df.iloc[:,1:]
-
-
-
-  # ## I am using here KFold method to train and test my model to get good metrices and performance
-
-  # In[25]:
-
-
-  kf=KFold(n_splits=5,shuffle=True,random_state=2)
-  rmse=[]
-  for train,test in kf.split(x,y):
-      LR=LinearRegression()
-      #print(train)
-      xtrain=x.iloc[train]
-      xtest=x.iloc[test]
-      ytrain=y.iloc[train]
-      ytest=y.iloc[test]
-      LR.fit(xtrain,ytrain)
-      ypredict=LR.predict(xtest)
-      
-      rmse.append(np.sqrt(metrics.mean_squared_error(ytest,ypredict)))
-      
-
-  print('Rmse error in the first test is :%1.3f'%(rmse[0]))
-  print('Rmse error in the second test is:%1.3f'%(rmse[1]))
-  print('Rmse error in the third test is :%1.3f'%(rmse[2]))
-  print('Rmse error in the fourth test is:%1.3f'%(rmse[3]))
-  print('Rmse error in the fifth test is :%1.3f'%(rmse[4]))
-
-  print('Average mean rmse error i.e Bias error is: %1.3f'%(np.mean(rmse)))
-
-  print('Variance of rmse error i.e Variance error is: %1.3f'%(np.var(rmse,ddof=1)))
-
-      
-      
-      
-
-
-  # In[26]:
-
-
-  ## predicting the mileage values for new values 
-  values=[[8,206,200,1900,70,2]]
-  new_mileage=LR.predict(values)
-  print('predicted new mileage for above values is %1.3f'%new_mileage)
-  return LR.predict(values)
-
-
-  # In[27]:
-
-import streamlit as st 
-st.title("Mileage Predictor")
+st.title("Mileage Predictor of Vehicle")
 st.markdown("##### Want to check mileage of you chosen car?\n##### So let's try evaluating the mileage ")
 st.image("mileage.jpg")
+st.markdown("#### Fill in the details below and get your predicted mileage of the car #####")
 
 
-a=st.number_input("Enter the number of Cylinders in cc",0,10000)
-b=st.number_input("Enter the Displacement in cc",0,10000)
-c=st.number_input("Enter the Horsepower in ps",100,50000)
-d=st.number_input("Enter the Weight in Kg",200,10000)
+a = st.number_input("Enter the number of Cylinders in cc of vehicle",20,1000)
+b = st.number_input("Enter the Displacement in cc of vehicle",500,999999)
+c = st.number_input("Enter the Horsepower in ps of vehicle",400,600000)
+d = st.number_input("Enter the Weight in Kg of vehicle",200,7890000)
 if st.button('Predict'):
-  out=mil([[a,b,c,d,70,20]])
+    out = mil([[a, b, c, d, 70, 20]])
 
-  st.write(out[0], 'is the predicted mileage in kmpl')
+    st.write(out[0], 'is the predicted mileage in kmpl')
 
 
 
